1d602156539d9d65da7127f394b141cd8f119da4
saltfile <3205617363@qq.com>
2023-01-01 23:03:21 +0800

    01-01

--- test_udp/src/structs/MyHeap.java
+++ test_udp/src/structs/MyHeap.java
@@ -2,7 +2,7 @@ package structs;
 
 import java.util.Arrays;
 
-public class MyHeap<E> {
+public class MyHeap {
 
     private static final int DEFAULT_INITIAL_CAPACITY = 11;
 
@@ -14,10 +14,12 @@ public class MyHeap<E> {
         queue = new Object[DEFAULT_INITIAL_CAPACITY];
     }
 
-    public boolean offer(E e) {
+    public boolean offer(Integer e) {
         if (e == null)
             throw new NullPointerException();
+
         int i = size;
+
         if (i >= queue.length) {
             grow(i + 1);
         }
@@ -44,53 +46,55 @@ public class MyHeap<E> {
         queue = Arrays.copyOf(queue, newCapacity);
     }
 
-    private void siftUp(int k, E x) {
+    private void siftUp(int k, Integer x) {
         siftUpComparable(k, x);
     }
 
-    private void siftUpComparable(int k, E x) {
-//        logger.info("【入队】元素：{} 当前队列：{}", JSON.toJSONString(x), JSON.toJSONString(queue));
+    private void siftUpComparable(int k, Integer x) {
+
         while (k > 0) {
-            // 获取父节点Idx，相当于除以2
+            //找寻父节点位置
             int parent = (k - 1) >>> 1;
-//            logger.info("【入队】寻找当前节点的父节点位置。k：{} parent：{}", k, parent);
+            //找到父节点
             Object e = queue[parent];
-            // 如果当前位置元素，大于父节点元素，则退出循环
-            if (compareTo(x, (E) e) >= 0) {
-//                logger.info("【入队】值比对，父节点：{} 目标节点：{}", JSON.toJSONString(e), JSON.toJSONString(x));
+            //进行比较如果父节点大于目前节点就break将x放到当前节点
+            if (compareTo(x, (Integer) e) >= 0) {
+
                 break;
+
             }
-            // 相反父节点位置大于当前位置元素，则进行替换
-//            logger.info("【入队】替换过程，父子节点位置替换，继续循环。父节点值：{} 存放到位置：{}", JSON.toJSONString(e), k);
+
+            //如果小于就继续往上面找
             queue[k] = e;
             k = parent;
         }
         queue[k] = x;
-//        logger.info("【入队】完成 Idx：{} Val：{} \r\n当前队列：{} \r\n", k, JSON.toJSONString(x), JSON.toJSONString(queue));
+
     }
 
-    public boolean add(E e) {
+    public boolean add(Integer e) {
+
         return offer(e);
     }
 
-    public E poll() {
+    public Integer poll() {
         if (size == 0)
             return null;
         int s = --size;
-        E result = (E) queue[0];
-        E x = (E) queue[s];
+        Integer result = (Integer) queue[0];
+        Integer x = (Integer) queue[s];
         queue[s] = null;
         if (s != 0)
             siftDown(0, x);
         return result;
     }
 
-    private void siftDown(int k, E x) {
+    private void siftDown(int k, Integer x) {
         siftDownComparable(k, x);
     }
 
     @SuppressWarnings("unchecked")
-    private void siftDownComparable(int k, E x) {
+    private void siftDownComparable(int k, Integer x) {
         // 先找出中间件节点
         int half = size >>> 1;
         while (k < half) {
@@ -99,12 +103,12 @@ public class MyHeap<E> {
             Object c = queue[child];
             int right = child + 1;
             // 左子节点与右子节点比较，取最小的节点
-            if (right < size && compareTo((E) c, (E) queue[right]) > 0) {
+            if (right < size && compareTo((Integer) c, (Integer) queue[right]) > 0) {
 //                logger.info("【出队】左右子节点比对，获取最小值。left：{} right：{}", JSON.toJSONString(c), JSON.toJSONString(queue[right]));
                 c = queue[child = right];
             }
             // 目标值与c比较，当目标值小于c值，退出循环。说明此时目标值所在位置适合，迁移完成。
-            if (compareTo(x, (E) c) <= 0) {
+            if (compareTo(x, (Integer) c) <= 0) {
                 break;
             }
             // 目标值小于c值，位置替换，继续比较
@@ -117,11 +121,44 @@ public class MyHeap<E> {
         queue[k] = x;
     }
 
-    public E peek() {
-        return (size == 0) ? null : (E) queue[0];
+    public Integer peek() {
+        return (size == 0) ? null : (Integer) queue[0];
     }
 
-    public int compareTo(E firstElement, E secondElement) {
-        throw new RuntimeException("未实现 compareTo 方法");
+    public int compareTo(Integer firstElement, Integer secondElement) {
+        return firstElement.compareTo(secondElement);
     }
+
+
+    public static void main(String[] args) {
+
+        int[] a = new int[]{1,2,3,4,5,6,7};
+
+        int k = 3;
+        //父节点寻找方式
+        int n = k >>> 1;
+
+        int u = 2;
+        //左节点寻找方式
+        int s = (u << 1) + 1;
+
+        System.out.println(a[u]+" "+a[s]);
+
+//        MyHeap myHeap = new MyHeap();
+//        myHeap.add(1);
+//        myHeap.add(2);
+//        myHeap.add(3);
+//        myHeap.add(0);
+//        myHeap.add(5);
+//        myHeap.add(6);
+
+
+
+
+
+
+    }
+
+
+
 }
